7c0a87771881fc55613a759f2890a9ea96e0445e
LuminoWave <127996551+LuminoWave@users.noreply.github.com>
2023-04-12 18:29:43 -0400

    Add files via upload

---
+++ shopping-cart/script.js
@@ -0,0 +1,11 @@
+function addItem() {}
+
+document.getElementById('add-to-cart').addEventListener('click', () => {
+  const itemName = document.getElementById('item-name').value;
+  const itemPrice = document.getElementById('item-price').value;
+});
+
+function checkout() {}
+
+// Add an event listener to the "Checkout" button
+document.getElementById('checkout').addEventListener('click', checkout);
