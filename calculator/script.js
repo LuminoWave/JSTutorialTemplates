7242a2666da322d1bba167436c843953a1f130ee
LuminoWave <127996551+LuminoWave@users.noreply.github.com>
2023-05-11 13:27:06 -0400

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,16 @@
+function plusOperation() {
+    // TODO: Implement the logic for addition.
+  }
+  
+  function minusOperation() {
+    // TODO: Implement the logic for subtraction.
+  }
+  
+  function multiplyOperation() {
+    // TODO: Implement the logic for multiplication.
+  }
+  
+  function divideOperation() {
+    // TODO: Implement the logic for division.
+  }
+  
